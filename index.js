23d6d90503dc77d44dd6e3576649dafc05894da0
amich <ameetmgr10@gmail.com>
2025-08-28 14:06:58 +0545

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import app from "./src/config/express.config.js";
 const httpServer = http.createServer(app);
 const PORT = 9006;
 
-httpServer.listen(PORT,'127.0.0.1',(err) =>{
+httpServer.listen(PORT,'0.0.0.0',(err) =>{
     if(!err){
         console.log(`The server is runing on PORT:${PORT}`)
         console.log(`Press ctrl + C to exit the server`)
