002c46503253ea12fedb910e69fdbcc4f7dff535
amich <ameetmgr10@gmail.com>
2025-08-28 13:52:42 +0545

    updated

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blog-frontend-nu-ten.vercel.app/",
     credentials: true, // needed if using cookies or Authorization headers
   })
 );
