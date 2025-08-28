269dcd6c09e610bb24d13b5c2a944a75425c71dd
amich <ameetmgr10@gmail.com>
2025-08-28 12:48:47 +0545

    updated

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,7 +4,7 @@ import { mongoDbConfig } from "./constants.js";
 const dbInit = async () => {
   try {
     console.log(process.env.MONGODB_URL)
-    await mongoose.connect(process.env.MONGODB_URL, {
+    await mongoose.connect("mongodb+srv://amich:amich@cluster0.cff3p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       dbName: mongoDbConfig.mongoDbName,
       autoCreate: true,
       autoIndex: true,
